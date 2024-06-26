facd8104b87a1a9e9f44e525a9f0f5043c46e60f
GuidoFlores <96491039+LeoParra03@users.noreply.github.com>
2024-06-26 10:00:14 -0500

    dashboard

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+
+import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
+import Indicator from './components/Indicator';
+import Summary from './components/Summary';
+import BasicTable from './components/BasicTable';
+import WeatherChart from './components/WeatherChart';
+import ControlPanel from './components/ControlPanel';
+
+import './App.css'
+
+function App() {
+
+  return (
+    <Grid container spacing={5}>
+      <Grid xs={12} sm={4} md={3} lg={2}> <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} /></Grid>
+      <Grid xs={6} sm={4} md={3} lg={2}>  <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} /></Grid>
+      <Grid xs={6} sm={4} md={3} lg={2}>  <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} /></Grid>
+      <Grid xs={12} sm={4} md={3} lg={2}><Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} /></Grid>
+      <Grid xs={6} sm={4} md={6} lg={2}><Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} /></Grid>
+      <Grid xs={6} sm={4} md={6} lg={2}><Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} /></Grid>
+      <Grid xs={12} sm={4} md={3} lg={3}><Summary></Summary></Grid>
+      <Grid xs={12} md={6} lg={9} >
+	       <BasicTable />
+	    </Grid> 
+     
+         <Grid xs={12} lg={2}>
+             <ControlPanel />
+         </Grid>
+         <Grid xs={12} lg={10}>
+             <WeatherChart></WeatherChart>
+         </Grid>
+    </Grid>
+  )
+}
+
+export default App
